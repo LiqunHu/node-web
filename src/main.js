4f135bcb9153ffad6bd6bbf2697a4ed732e1a779
huliqun <huliquns@126.com>
2018-06-11 16:55:11 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
 import 'admin-lte/dist/css/AdminLTE.min.css'
 import 'admin-lte/dist/css/skins/_all-skins.min.css'
+import './assets/css/ui.css'
 
 Vue.config.productionTip = false
 
