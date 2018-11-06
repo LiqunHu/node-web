60c5123aa046625066618037e890c027a9e4e4d5
huliqun <huliquns@126.com>
2018-11-06 14:52:06 +0800

    f

--- config/index.js
+++ config/index.js
@@ -11,13 +11,13 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api': {
+      '/v1/api': {
         target: 'http://localhost:9090', // 接口的域名
-        // secure: false,  // 如果是https接口，需要配置这个参数
-        // changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
-        // pathRewrite: {
-        //   '^/api': ''
-        // }
+        secure: false,  // 如果是https接口，需要配置这个参数
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        pathRewrite: {
+          '^/v1/api': '/api'
+        }
       },
       '/temp': { target: 'http://localhost:9090'},
       '/files': { target: 'http://localhost:9090'},
